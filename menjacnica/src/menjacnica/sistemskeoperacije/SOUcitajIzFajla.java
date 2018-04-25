57ac823153d7a8b709286019815367b1d28ff769
sreckoN <srecko.nikolic71@hotmail.com>
2018-04-25 18:09:50 +0200

    Sredjen logicki nivo

---
+++ menjacnica/src/menjacnica/sistemskeoperacije/SOUcitajIzFajla.java
@@ -0,0 +1,23 @@
+package menjacnica.sistemskeoperacije;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.LinkedList;
+
+import menjacnica.Valuta;
+
+public class SOUcitajIzFajla {
+
+	public static LinkedList<Valuta> izvrsi(String putanja, LinkedList<Valuta> kursnaLista) {
+		try{
+			ObjectInputStream in = new ObjectInputStream(
+					new BufferedInputStream(new FileInputStream(putanja)));
+			
+			return (LinkedList<Valuta>)(in.readObject());
+		}catch(Exception e){
+			throw new RuntimeException(e);
+		}
+	}
+	
+}
